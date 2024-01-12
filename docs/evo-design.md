ceb5e93770362999ec37780cfad86f5e0faf1018
lancerstadium <lancerstadium@163.com>
2024-01-12 15:57:44 +0800

    Step2: 加法与减法

--- docs/evo-design.md
+++ docs/evo-design.md
@@ -1,4 +1,26 @@
 # Language design of evo
 
 
+## Step 1: 整数输出
 
+```
+.intel_syntax noprefix
+.globl main
+main:
+        mov rax, 5
+        add rax, 20
+        sub rax, 4
+        ret
+```
+
+## Step 2: 加法与减法
+
+```
+.intel_syntax noprefix
+.globl main
+main:
+  mov rax, 5
+  add rax, 20
+  sub rax, 4
+  ret
+```
--- src/evoc/evoc.c
+++ src/evoc/evoc.c
@@ -6,10 +6,29 @@ int main(int argc, char **argv) {
         fprintf(stderr, "usage: %s <int>\n", argv[0]);
         return 1;
     }
-    printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
-    printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
-    printf("  ret\n");
+
+    char *p = argv[1];
+
+    printf(".intel_syntax noprefix\n");                     // 设置汇编语法格式：INTEL，无前缀
+    printf(".globl main\n");                                // 定义全局变量：main
+    printf("main:\n");                                      // 定义main函数
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));          // mov rax, argv[1]
+
+    while(*p) {
+        if(*p == '+') {                                     // 加号：+
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));  // add rax, argv[3]
+            continue;
+        }
+        if(*p == '-') {                                     // 减号：-
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));  // sub rax, argv[3]
+            continue;
+        }
+
+        fprintf(stderr, "unknown operator: %c\n", *p);      // 未知运算符
+        return 1;
+    }
+    printf("  ret\n");                                      // 返回
     return 0;
 }
