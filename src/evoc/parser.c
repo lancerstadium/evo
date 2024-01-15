4e6f76883f9ed78131681336d12c39dcb0a090f5
lancerstadium <lancerstadium@163.com>
2024-01-15 20:44:26 +0800

    Step10: 分号语句;

--- src/evoc/parser.c
+++ src/evoc/parser.c
@@ -10,13 +10,31 @@
 // ==================================================================================== //
 
 // parser语法分析：创建新节点
-static Node* node_new(NodeType type);
+static Node* node_new(NodeType type) {
+    Node *node = (Node*)calloc(1, sizeof(Node));
+    node->type = type;
+    return node;
+}
 // parser语法分析：创建二元运算节点
-static Node* node_new_binary(NodeType type, Node *lhs, Node *rhs);
+static Node* node_new_binary(NodeType type, Node *lhs, Node *rhs) {
+    Node* node = node_new(type);
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
 // parser语法分析：创建一元运算节点
-static Node* node_new_unary(NodeType type, Node *expr);
+static Node* node_new_unary(NodeType type, Node *expr) {
+    Node* node = node_new(type);
+    node->lhs = expr;
+    return node;
+}
 // parser语法分析：创建数字节点
-static Node* node_new_num(int val);
+static Node* node_new_num(int val) {
+    Node* node = node_new(ND_NUM);
+    node->val = val;
+    return node;
+}
+
 static Node* stmt(Token **rest, Token *tok);
 static Node* expr_stmt(Token **rest, Token *tok);
 static Node* expr(Token **rest, Token *tok);
@@ -145,31 +163,7 @@ static Node *prim(Token **rest, Token *tok) {
 //                                    Pub API: parser
 // ==================================================================================== //
 
-// parser语法分析：创建新节点
-Node* node_new(NodeType type) {
-    Node *node = (Node*)calloc(1, sizeof(Node));
-    node->type = type;
-    return node;
-}
-// parser语法分析：创建二元运算节点
-Node* node_new_binary(NodeType type, Node *lhs, Node *rhs) {
-    Node* node = node_new(type);
-    node->lhs = lhs;
-    node->rhs = rhs;
-    return node;
-}
-// parser语法分析：创建一元运算节点
-Node* node_new_unary(NodeType type, Node *expr) {
-    Node* node = node_new(type);
-    node->lhs = expr;
-    return node;
-}
-// parser语法分析：创建数字节点
-Node* node_new_num(int val) {
-    Node* node = node_new(ND_NUM);
-    node->val = val;
-    return node;
-}
+
 // parser语法分析：解析
 Node* evoc_parse(Token *tok) {
     Node head = {};
