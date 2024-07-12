d3e8a52f2dd01eb23d0ed5a60978c201023563d4
lancerstadium <lancerstadium@163.com>
2024-07-12 22:45:51 +0800

    2024-07-12 22:45:51

--- evo/op/dft/Conv.c
+++ evo/op/dft/Conv.c
@@ -1,7 +1,31 @@
-
 #include "../../core/resolver.h"
 
+typedef enum {
+    AUTO_PAD_NOTSET = 0,
+    AUTO_PAD_SAME_UPPER = 1,
+    AUTO_PAD_SAME_LOWER = 2,
+    AUTO_PAD_VALID = 3,
+} auto_pad_t;
+
+typedef enum {
+    CONV_SIMPLE = 0,
+    CONV_CACHED = 1,
+    CONV_IM2COL = 2,
+} conv_mode_t;
+
+typedef struct {
+    auto_pad_t auto_pad;
+    int group;
+    int* kernels;
+    int nkernel;
+    int* dilations;
+    int ndilation;
+    int* pads;
+    int npad;
+    int* strides;
+    int nstride;
+    int cpads[32];
+} operator_pdata_t;
 
 void op_Conv_dft(node_t*) {
-    
 }
